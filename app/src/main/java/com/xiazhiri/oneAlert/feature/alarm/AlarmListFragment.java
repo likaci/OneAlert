b2577f9fef7fd3eaf00794cf7f1a8af686e0f188
likaci <likaci@qq.com>
2016-04-02 16:41:45 +0800

    根据不同状态过滤报警信息

--- app/src/main/java/com/xiazhiri/oneAlert/feature/alarm/AlarmListFragment.java
+++ app/src/main/java/com/xiazhiri/oneAlert/feature/alarm/AlarmListFragment.java
@@ -1,20 +1,26 @@
 package com.xiazhiri.oneAlert.feature.alarm;
 
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 
 import com.xiazhiri.oneAlert.R;
 import com.xiazhiri.oneAlert.feature.base.BaseFragment;
 import com.xiazhiri.oneAlert.model.Alarm;
 import com.xiazhiri.oneAlert.network.OneAlertService;
 
+import java.util.HashMap;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.schedulers.Schedulers;
@@ -27,6 +33,8 @@ public class AlarmListFragment extends BaseFragment {
 
     private String mParam1;
     private String mParam2;
+    private int processRadioId = R.id.processAll;
+    private int assignRadioId = R.id.assignAll;
 
     public AlarmListFragment() {
     }
@@ -58,7 +66,39 @@ public class AlarmListFragment extends BaseFragment {
     }
 
     private void initData() {
-        OneAlertService.getAlarm()
+
+        String assign;
+        if (assignRadioId == R.id.assignMine) {
+            assign = "alert/assigned";
+        } else {
+            assign = "alert";
+        }
+
+        String processState;
+        switch (processRadioId) {
+            case R.id.processAll:
+                processState = null;
+                break;
+            case R.id.processNot:
+                processState = "ACTIVE";
+                break;
+            case R.id.processDoing:
+                processState = "ACK";
+                break;
+            case R.id.processDone:
+                processState = "CLOSED";
+                break;
+            default:
+                processState = null;
+                break;
+        }
+
+        HashMap<String, String> queryMap = new HashMap<>();
+        queryMap.put("UPYOO_TOKEN", "f05fce9a-0b64-402d-8d7b-283cb04bb31f");
+        if (processState != null) {
+            queryMap.put("status", processState);
+        }
+        OneAlertService.getAlarm(assign, queryMap)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Action1<Alarm>() {
@@ -85,5 +125,29 @@ public class AlarmListFragment extends BaseFragment {
         ButterKnife.unbind(this);
     }
 
+
+    @OnClick(R.id.fab)
+    public void onFabClick() {
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_alarm_filter, null);
+        final RadioGroup process = (RadioGroup) view.findViewById(R.id.process);
+        process.check(processRadioId);
+        final RadioGroup assign = (RadioGroup) view.findViewById(R.id.assign);
+        assign.check(assignRadioId);
+
+        new AlertDialog.Builder(getActivity())
+                .setTitle("过滤条件")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        AlarmListFragment.this.processRadioId = process.getCheckedRadioButtonId();
+                        AlarmListFragment.this.assignRadioId = assign.getCheckedRadioButtonId();
+                        initData();
+                    }
+                })
+                .setNegativeButton("取消", null)
+                .setView(view)
+                .create()
+                .show();
+    }
 
 }
--- app/src/main/java/com/xiazhiri/oneAlert/network/OneAlertService.java
+++ app/src/main/java/com/xiazhiri/oneAlert/network/OneAlertService.java
@@ -3,10 +3,14 @@ package com.xiazhiri.oneAlert.network;
 import com.xiazhiri.oneAlert.model.Alarm;
 import com.xiazhiri.oneAlert.model.ChartData;
 
+import java.util.Map;
+
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.QueryMap;
 import rx.Observable;
 
 /**
@@ -16,8 +20,8 @@ public class OneAlertService {
 
     public static final String BASE_URL = "http://c.110monitor.com/";
 
-    public static Observable<Alarm> getAlarm() {
-        return SingletonHolder.SERVICE.getAlarm();
+    public static Observable<Alarm> getAlarm(String assign, Map<String,String> queryMap) {
+        return SingletonHolder.SERVICE.getAlarm(assign, queryMap);
     }
 
     public static Observable<ChartData> getChartData() {
@@ -25,8 +29,8 @@ public class OneAlertService {
     }
 
     public interface OneAlertServiceApi {
-        @GET("http://c.110monitor.com/alert/api/alert?UPYOO_TOKEN=8cde4c8d-0ef8-41bc-87cd-aa0744306275")
-        Observable<Alarm> getAlarm();
+        @GET("http://c.110monitor.com/alert/api/{assign}")
+        Observable<Alarm> getAlarm(@Path(value = "assign", encoded = true) String assign, @QueryMap Map<String, String> queryMap);
 
         //http://c.110monitor.com/console//CrossDomainProxy?1=1&_workId=1459526259772-49412&UPYOO_TOKEN=98ba632b-7c6a-4894-96cc-19bf8db1721a&_url=http://c.110monitor.com/analyse/api/alertAnalyse?startTime=1459267059768&endTime=1459526259768&_=1459526206604
         @GET("http://c.110monitor.com/console//CrossDomainProxy?1=1&_workId=1459566479531-64875&UPYOO_TOKEN=98ba632b-7c6a-4894-96cc-19bf8db1721a&_url=http://c.110monitor.com/analyse/api/alertAnalyse?startTime=1459307279525&endTime=1459566479525&_=1459564554785")
